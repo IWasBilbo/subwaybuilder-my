06e28814a86268afa21d3862cb718b574a3cfcc3
IWasBilbo <markanze@me.com>
2025-10-17 13:18:33 +0200

    sync

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const config = {
-  "subwaybuilderLocation": "C:\\Users\\piero\\AppData\\Local\\Programs\\Subway\ Builder\\", // appimage location image on linux or install directory on windows (something like C:\\Users\\[username]\\AppData\\Local\\Programs\\Subway\ Builder)
+  "subwaybuilderLocation": "/Applications/Subway Builder.app/", // appimage location image on linux or install directory on windows (something like C:\\Users\\[username]\\AppData\\Local\\Programs\\Subway\ Builder)
   "places": [
     {
       "code": "LJU",
       "name": "Ljubljana",
       "description": "Sometimes, the most beautiful city in the world.",
       "bbox": [13.800647,45.750067,14.777057,46.392204], // -79.454498,43.624458,-79.310818,43.680412
-      "population": 750000,
+      "population": 1000000,
     },
   ],
   "platform": "macos"
